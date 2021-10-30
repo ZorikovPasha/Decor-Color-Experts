3bcdc182e74c829748b494182557d8af5bd8cb86
ZorikovPasha <zorikov.pasha@mail.ru>
2021-10-30 22:04:02 +0300

    added sections portfolio survey catalog reviews

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,1 +1,10 @@
-console.log('working')
+$(function() {
+  $('.item__label').on('click', function() {
+    if ( !$(this).hasClass('active') ) {
+      $(this).siblings().each(function(idx, label) {
+        $(label).removeClass('active')
+      })
+      $(this).addClass('active')
+    }
+  })
+})
